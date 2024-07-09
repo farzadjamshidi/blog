22a43eb643100012e21ef1c8d7ab39265284214d
farzadjamshidi <farzadjamshidicom@yahoo.com>
2024-07-09 12:52:17 +0100

    Fix swagger by AddSwaggerGen

--- Blog.API/Registrars/SwaggerRegistrar.cs
+++ Blog.API/Registrars/SwaggerRegistrar.cs
@@ -7,6 +7,7 @@ public class SwaggerRegistrar : IWebApplicationBuilderRegistrar
 {
     public void RegisterServices(WebApplicationBuilder builder)
     {
+        builder.Services.AddSwaggerGen();
         builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
     }
 }
